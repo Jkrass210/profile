1bb23e015c62077e05b44d937d56bd1fb73adcbe
Evgeniy <zoo_cry@mail.ru>
2024-03-29 23:04:30 +0600

    new add

--- docs/js/main.js
+++ docs/js/main.js
@@ -28,6 +28,55 @@ function activationInput(input, placeholder) {
  }
 }
 
+function activationNightTeme () {
+  if(document.body.classList.contains('night-hteme')) {
+    document.body.classList.remove('night-hteme')
+   } else if (!document.body.classList.contains('night-hteme')) {
+    document.body.classList.add('night-hteme')
+   }
+}
+
+document.querySelectorAll(".js-scroll-link").forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const href = this.getAttribute("href").substring(1);
+    const scrollTarget = document.getElementById(href);
+    const elementPosition = scrollTarget.getBoundingClientRect().top;
+
+    window.scrollBy({
+      top: elementPosition,
+      behavior: "smooth",
+    });
+  });
+});
+
+const resizableSwiper = (breakpoint, swiperClass, swiperSettings, callback) => {
+ let swiper;
+
+ breakpoint = window.matchMedia(breakpoint);
+
+ const enableSwiper = function(className, settings) {
+   swiper = new Swiper(className, settings);
+
+   if (callback) {
+     callback(swiper);
+   }
+ }
+
+ const checker = function() {
+   if (breakpoint.matches) {
+     return enableSwiper(swiperClass, swiperSettings);
+   } else {
+     if (swiper !== undefined) swiper.destroy(true, true);
+     return;
+   }
+ };
+
+ breakpoint.addEventListener('change', checker);
+ checker();
+}
+
 btnAboutMe.addEventListener('click', function() {
  activationDesc(aboutMe, aboutTools, btnAboutMe, btnAboutTools)
 });
@@ -49,10 +98,12 @@ btnDayNight.addEventListener('click', function() {
  const svgDay = btnDayNight.querySelector('.btn__day');
  const svgNight = btnDayNight.querySelector('.btn__night');
  if(spanBtn.classList.contains('btn__span-night')) {
+  activationNightTeme()
   spanBtn.classList.remove('btn__span-night')
   svgDay.classList.add('btn__svg--hidden')
   svgNight.classList.remove('btn__svg--hidden')
  } else {
+  activationNightTeme()
   spanBtn.classList.add('btn__span-night')
   svgNight.classList.add('btn__svg--hidden')
   svgDay.classList.remove('btn__svg--hidden')
@@ -63,10 +114,25 @@ const mobMenu = header.querySelector('.header__wrapper-menu');
 
 burger.addEventListener('click', function(){
  mobMenu.classList.add('header__wrapper-menu--active')
+ document.body.classList.add("stop-scroll");
 });
 
 closeBurger.addEventListener('click', function(){
  mobMenu.classList.remove('header__wrapper-menu--active')
+ document.body.classList.remove("stop-scroll");
 });
 
-
+resizableSwiper('(max-width: 950px)', '.skillbox__swiper', {
+  loop: false,
+  spaceBetween: 25,
+  slidesPerView: 1.5,
+  loop: false,
+  freeMode: false,
+  breakpoints: {
+    700: {
+      spaceBetween: 25,
+      slidesPerView: 2.5,
+    },
+  },
+ }
+);
